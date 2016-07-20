3c4d6732ad94f12d2c4a119b5e53cae36b7c0a1e
panditdres <pandit.dres@gmail.com>
2016-07-20 18:24:39 -0400

    Changes to partial shop page

--- app/site/controllers/shop.ctrl.js
+++ app/site/controllers/shop.ctrl.js
@@ -40,15 +40,15 @@
 
 		shopVm.selectFilter = selectFilter; 
 		
-		function selectFilter(products) {
-			console.log(products);
+		function selectFilter(item) {
+			console.log(item);
 				if (shopVm.sunCheck == true) {
-					if (products.category == "Sun Glasses") {
+					if (item.category == "Sun Glasses") {
 						console.log('sunglass');
 						return true
 					}
 				} else if (shopVm.eyeCheck == true) {
-					if (products.category == "Eye Glasses") {
+					if (item.category == "Eye Glasses") {
 						console.log('eyeglass')
 						return true
 					}
